38a2405743b52e60c4ff08f2f74fd9a74b5e476c
James Cotton <peabody124@gmail.com>
2013-01-23 01:03:50 -0600

    Create initial formula for installing kicad
    Required a patch since for some reason kicad disables the aui
    module for wxWidgets which is required for compilation.

---
+++ Library/Formula/kicad.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class Kicad < Formula
+  head 'https://github.com/peabody124/kicad.git'
+  homepage 'https://launchpad.net/kicad'
+  version '0.1'
+  sha1 ''
+
+  depends_on 'cmake' => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on 'wxwidgets'
+
+  def patches
+    # fixes wx-config not requiring aui module
+    "https://gist.github.com/raw/4602653/0e4397884062c8fc44a9627e78fb4d2af20eed5b/gistfile1.txt"
+  end
+
+  def install
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    #system "cmake", ".", "-DwxWidgets_CONFIG_EXECUTABLE=/usr/local/bin/wx-config -DwxWidgets_wxrc_EXECUTABLE=/usr/local/bin/wxrc -DKICAD_TESTING_VERSION=ON -DCMAKE_CXX_FLAGS=-D__ASSERTMACROS__", *std_cmake_args
+    system "cmake", ".", "-DKICAD_STABLE_VERSION=ON -DCMAKE_CXX_FLAGS=-D__ASSERTMACROS__", *std_cmake_args
+    system "make install" # if this fails, try separate make/make install steps
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test kicad`.
+    system "false"
+  end
+end
